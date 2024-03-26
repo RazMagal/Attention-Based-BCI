8ab1afb963d7715d2d1f954a5090bdae289f2c22
RazMagal <102611420+RazMagal@users.noreply.github.com>
2024-03-26 13:10:37 +0200

    Delete Unnecessary files

--- primitive_demo/pickle/test_pickle.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import os
-# Construct the path to the data directory and the pickle file
-default_data_path = os.path.join(os.getcwd(), 'clip_eeg.pickle')
-df = pd.read_pickle(default_data_path)
-print(df.shape)
