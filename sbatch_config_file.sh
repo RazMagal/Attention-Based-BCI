0cc540e7507d0e735f3bc186c4f0353ad6bcf97a
RazMagal <102611420+RazMagal@users.noreply.github.com>
2024-05-21 13:15:26 +0300

    more_resources

--- sbatch_config_file.sh
+++ sbatch_config_file.sh
@@ -3,9 +3,9 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=raz.magal@mail.huji.ac.il     # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=2gb                     # Job memory request
+#SBATCH --mem=32gb                    # Job memory request
 #SBATCH -c2			      # Job memory request
-#SBATCH --gres=gpu:1,vmem:10g         # Job memory request
+#SBATCH --gres=gpu:2,vmem:10g         # Job memory request
 #SBATCH --time=00:05:00               # Time limit hrs:min:sec
 #SBATCH --output=Logs/serial_test_%j.log   # Standard output and error log
 pwd; hostname; whoami; groups; date
