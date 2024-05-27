e8e012f33502106144d021663dd75c5a06e6e638
aranweizman <aran.weizman@brightmerge.com>
2024-05-27 14:31:20 +0300

    updated MLP to convergent version

--- sanity_labs/MLP/MLP.py
+++ sanity_labs/MLP/MLP.py
@@ -1,39 +1,20 @@
+import torch
 import torch.nn as nn
 
 
 class PolyMod(nn.Module):
-    def __init__(self, topology):
-        """
-        This function sets up the netless_data, via the given topology.
-
-        Activation:
-        The activation function for each layer is RELU (experimental: counter-measure for vanishing gradient).
-        The activation function for the output layer is sigmoid (output must be boolean).
-
-        :topology: list of lengths of the layers (number of neurons in each layer).
-        """
-
-        super(PolyMod, self).__init__()
-
-        # unpack topology
-        pep_chain_length, HL1_length, HL2_length, HL3_length = topology
-
-        relu_act = nn.ReLU()
-        sig = nn.Sigmoid()
-
-        forward_seq_list = [nn.Linear(pep_chain_length, HL1_length, bias=True), relu_act,
-                            nn.Linear(HL1_length, HL2_length, bias=True), relu_act,
-                            nn.Dropout(p=0.2),
-                            nn.Linear(HL2_length, HL3_length, bias=True), relu_act,
-                            nn.Dropout(p=0.2),
-                            nn.Linear(HL3_length, 1, bias=True), sig]
-
-        self.forward_seq = nn.Sequential(*forward_seq_list)
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(180, 72),
+            nn.ReLU(),
+            nn.Linear(72, 16),
+            nn.ReLU(),
+            nn.Linear(16, 2),
+        )
 
     def forward(self, x):
-        """
-        This method computes a forward pass through the layers.
-        """
-        binary_output = self.forward_seq(x)
-
-        return binary_output
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
--- sanity_labs/MLP/train_MLP.py
+++ sanity_labs/MLP/train_MLP.py
@@ -1,126 +1,98 @@
-import os
-from datetime import datetime
-
-import numpy as np
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-
-# local imports
-from IDL_ex1.Ex1.data.preprocess_pepdata import setup_preprocessed_data
-from peptideMLP import PolyMod
-from IDL_ex1.Ex1.data.visual.metrics_monitor import plot_loss
-
-from typing import List
+from torch.utils.data import ConcatDataset, DataLoader
+from MLP import PolyMod
+from dataset import PeptideDataset
+from matplotlib import pyplot as plt
 
 
-def save_epoch(model: nn.Module, loss_trackers: List[np.ndarray], weights_tracker: List = None, model_name=''):
-    """
-    This function saves the state of the model (model parameters) in a directory,
-    which is created for the epoch.
+def get_sampler(npos, nneg):
 
-    The name of the (inner) directory is the date and time of it's creation
-    """
-
-    epochs_dir = '../../data/epochs'
-    # create epochs directory if one doesn't already exist
-    if not os.path.exists(epochs_dir):
-        os.mkdir(epochs_dir)
-
-    # create directory for the current epoch
-    epoch_datetime = datetime.now().strftime("%x_%X").replace('/', '.').replace(':', '-')
-    epoch_dir = os.path.join(epochs_dir, epoch_datetime)
-
-    # testing model -> create inner directory for test results
-    if model_name:
-        epoch_dir = os.path.join(epochs_dir, model_name, 'test')
-        os.mkdir(epoch_dir)
-
-    # training model -> save model and all it's iterations
-    else:
-        os.mkdir(epoch_dir)
-        # save model
-        torch.save(model, os.path.join(epoch_dir, 'epoch'))
-
-        # save model parameters if they are given
-        if weights_tracker:
-            os.makedirs(os.path.join(epoch_dir, 'parameter_iterations'))
-            for i, param in enumerate(weights_tracker):
-                torch.save(param, os.path.join(epoch_dir, 'parameter_iterations', f'{i}_params'))
-
-    # save loss
-    np.save(os.path.join(epoch_dir, 'neg_loss'), loss_trackers[0])
-    np.save(os.path.join(epoch_dir, 'pos_loss'), loss_trackers[1])
-
-    inner_dir_index = epoch_dir.rfind(os.path.sep)
-    return epoch_dir[inner_dir_index + 1:]
+    weights = 0.5 / torch.concat([torch.tensor([npos] * npos), torch.tensor([nneg] * nneg)]).double()
+    return torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
 
 
-def choo_choo(model, trainer, model_name=None):
-    """
-    This function trains the given model on the given data.
+batch_size = 64
 
-    The loss function (criterion) for the training process is the BCE function.
+# Create datasets
+data_pos = PeptideDataset("data", True)
+data_neg = PeptideDataset("data", False)
+data_pos_train, data_pos_test = data_pos.split(0.9)
+data_neg_train, data_neg_test = data_neg.split(0.9)
+data_pos_train_size, data_neg_train_size = len(data_pos_train), len(data_neg_train)
+data_train = ConcatDataset([data_pos_train, data_neg_train])
+data_test = ConcatDataset([data_pos_test, data_neg_test])
 
-    :pep_model: the model to be trained.
-    :pep_batch_tensor: tensor containing the batched data.
-    """
+# Create data loaders
+sampler_train = get_sampler(len(data_pos_train), len(data_neg_train))
+sampler_test = get_sampler(len(data_pos_test), len(data_neg_test))
+train_dataloader = DataLoader(data_train, sampler=sampler_train, batch_size=batch_size)
+test_dataloader = DataLoader(data_test, sampler=sampler_test)
 
-    print('training model')
-
-    # parameters
-    criterion = nn.BCELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2500, gamma=0.562)
-
-    i = 0
-    pos_loss_tracker = []
-    neg_loss_tracker = []
-    # parameter_tracker = [0] * len(trainer)
-
-    # run epoch
-    for x, train_label in iter(trainer):
-
-        # gradient must be zeroed at each iteration
-
-        y = model(x)
-        loss = criterion(y, train_label)
-
-        # collect analytics
-        if train_label:
-            pos_loss_tracker.append(loss.item())
-        else:
-            neg_loss_tracker.append(loss.item())
-
-        # weights = list(model.parameters())
-        # parameter_tracker[i] = weights
-
-        if not model_name:
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-
-        # log process
-        i += 1
-        if i % 1000 == 0:
-            print(f'[i]: {i} iterations passed')
-
-    # save model parameters
-    return save_epoch(model, [np.array(neg_loss_tracker), np.array(pos_loss_tracker)], model_name=model_name)
+# Create model
+model = PolyMod()
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.3, total_iters=15)
 
 
-if __name__ == '__main__':
-    train, test = setup_preprocessed_data('../../data')
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    train_loss = []
+    train_loss_ma = []
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-    MLP_topology = [9*20, 30, 30, 30]
-    pep_model = PolyMod(MLP_topology)
-    model_name = choo_choo(pep_model, train)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-    pep_model = torch.load(os.path.join('../../data', 'epochs', model_name, 'epoch'))
-    choo_choo(pep_model, test, model_name=model_name)
+        train_loss.append(loss.item())
+        if len(train_loss) >= 100:
+            train_loss_ma.append(sum(train_loss[-100:]) / 100)
 
-    plot_loss(os.path.join('../../data', 'epochs', model_name))
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+    return train_loss_ma
 
 
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            cur_loss = loss_fn(pred, y).item()
+            test_loss += cur_loss
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
+    return test_loss
+
+
+epochs = 15
+train_loss = []
+test_loss = []
+test_loss_xs = []
+for t in range(epochs):
+    print(f"Epoch {t+1}\n---------------------------")
+    train_loss += train(train_dataloader, model, loss_fn, optimizer)
+    test_loss.append(test(test_dataloader, model, loss_fn))
+    test_loss_xs.append(len(train_loss))
+    scheduler.step()
+print("Done!")
+
+# plotting code
+plt.plot(train_loss, label="Train")
+plt.plot(test_loss_xs, test_loss, label="Test")
+plt.scatter(test_loss_xs, torch.full((len(test_loss_xs),), fill_value=min(train_loss)), marker='|', label="Epochs")
+plt.xlabel(f"Batches seen (batch size = {batch_size})")
+plt.ylabel("Cross-Entropy loss")
+plt.title("Train and Test loss")
+plt.legend()
+plt.show()
